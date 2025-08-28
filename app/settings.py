64b9c6b4df926b2bce1250010579258e318588f5
Sam <dodobird181@gmail.com>
2025-08-28 12:38:05 -0400

    update installed apps in settings.py

--- app/settings.py
+++ app/settings.py
@@ -31,12 +31,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",  # required by allauth
+    # Third-party
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.microsoft",
+    "django_ses",
+    # Your apps
+    "subscriptions",
 ]
 
 MIDDLEWARE = [
